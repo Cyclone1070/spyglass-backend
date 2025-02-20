9c849e95b89e4479fb51555d7e3fac84ea139fa6
RyanMai <hoangmai1070@gmail.com>
2025-02-20 23:15:24 +1100

    added error handling to fetch_html

--- internal/scraper/fetch_html.go
+++ internal/scraper/fetch_html.go
@@ -1,15 +1,28 @@
 package scraper
 
 import (
+	"fmt"
+
 	"github.com/gocolly/colly/v2"
 )
 
 func FetchHTML(url string) string {
 	var responseBody string
+	var err string
 	collector := colly.NewCollector()
+
+	collector.OnError(func(r *colly.Response, e error) {
+		err = fmt.Sprintf("%d: %s", r.StatusCode, e.Error())
+	})
+
 	collector.OnHTML("html", func(e *colly.HTMLElement) {
 		responseBody, _ = e.DOM.Html()
 	})
+
 	collector.Visit(url)
-	return responseBody
+	if err == "" {
+		return responseBody
+	} else {
+		return err
+	}
 }
