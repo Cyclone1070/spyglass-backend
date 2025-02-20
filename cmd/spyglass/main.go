de208c26ddaa5a375f1a19642c5c18979a8301bc
RyanMai <hoangmai1070@gmail.com>
2025-02-20 19:03:19 +1100

    added colly to project

--- cmd/spyglass/main.go
+++ cmd/spyglass/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/Cyclone1070/spyglass-backend/internal/scraper"
+)
+
+func main() {
+	println(scraper.FetchHTML("http://quotes.toscrape.com/page/1/"))
+}
--- internal/scraper/fetch_html_test.go
+++ internal/scraper/fetch_html_test.go
@@ -11,12 +11,15 @@ import (
 
 func TestFetchData(t *testing.T) {
 	t.Run("make requests with valid formats to pages", func(t *testing.T) {
+		want := `<head></head>
+<body>content</body>`
+
 		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			io.WriteString(w, "request body")
+			io.WriteString(w, want)
 		}))
 		defer testServer.Close()
+
 		got := scraper.FetchHTML(testServer.URL)
-		want := "request body"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
