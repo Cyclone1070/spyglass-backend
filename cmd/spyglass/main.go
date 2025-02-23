02628fdef537c3d9f1d14d959d01db818bb738b3
RyanMai <hoangmai1070@gmail.com>
2025-02-23 23:18:17 +1100

    adding sorting by query

--- cmd/spyglass/main.go
+++ cmd/spyglass/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	links, err := scraper.FetchItems("https://www.imdb.com/find/?s=tt&q=test&ref_=nv_sr_sm")
+	links, err := scraper.FetchItems("https://www.imdb.com/find/?s=tt&q=test&ref_=nv_sr_sm", "test")
 	if err == nil {
 		for _, link := range links {
 			fmt.Printf("%s: %s\n", link.Title, link.Url)
--- internal/scraper/fetch_items.go
+++ internal/scraper/fetch_items.go
@@ -2,13 +2,16 @@ package scraper
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
 
-func FetchItems(url string) ([]Link, error) {
+func FetchItems(url string, query string) ([]Link, error) {
 	var links = []Link{}
 	var err error
+
+	normalizedQuery := strings.Split(strings.ToLower(query), " ")
 	collector := colly.NewCollector()
 
 	collector.OnError(func(r *colly.Response, e error) {
@@ -16,7 +19,18 @@ func FetchItems(url string) ([]Link, error) {
 	})
 
 	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		links = append(links, Link{e.Text, e.Attr("href")})
+		if strings.TrimSpace(e.Text) == "" || strings.TrimSpace(e.Attr("href")) == "" {
+			return
+		}
+
+		normalizedText := strings.ToLower(e.Text)
+
+		for _, word := range normalizedQuery {
+			if strings.Contains(normalizedText, word) {
+				links = append(links, Link{e.Text, e.Attr("href")})
+				break
+			}
+		}
 	})
 
 	collector.Visit(url)
