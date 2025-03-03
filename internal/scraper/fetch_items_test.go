69e8178e6aa4ee3bb7617ba906b67f7270736fc5
RyanMai <hoangmai1070@gmail.com>
2025-03-04 09:51:25 +1100

    added scraping based on cardPath

--- internal/scraper/fetch_items_test.go
+++ internal/scraper/fetch_items_test.go
@@ -2,6 +2,7 @@ package scraper_test
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -10,55 +11,64 @@ import (
 	"github.com/Cyclone1070/spyglass-backend/internal/scraper"
 )
 
-func TestFetchData(t *testing.T) {
-	t.Run("Return all links from page, sort out result matching query", func(t *testing.T) {
-		testCases := []string{"2", "sublink", "subdomain", "sub"}
-		for _, phrase := range testCases {
+func TestFetchItems(t *testing.T) {
+	type TestCase struct {
+		description string
+		top         string
+		bottom      string
+		cardPath    string
+		want        []scraper.CardContent
+	}
+	testCases := []TestCase{
+		{
+			description: "return links based on cardPath",
+			top:         "<div class='card'>",
+			bottom:      "</div>",
+			cardPath:    "html > body > div.container > div.card",
+			want: []scraper.CardContent{
+				{Title: "Example", Url: "https://example.com", OtherText: []string{}},
+				{Title: "Example 2", Url: "https://example.com/2", OtherText: []string{}},
+				{Title: "Test", Url: "https://test.com", OtherText: []string{}},
+			},
+		},
+	}
+	links := []string{"<a href='https://example.com'>Example</a>", "<a href='https://example.com/2'>Example 2</a>", "<a href='https://test.com'>Test</a>"}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
 			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintf(w, `<html>
-	<div class="container">
-		<div class="card">
-			<a href="https://example.com">Example</a>
-		</div>
-		<div class="card">
-			<a href="https://example.com/%s">Example %s</a>
-		</div>
-		<div class="card">
-			<a href="https://test.com">Test</a>
-		</div>
-		<div class="card">
-			<a href="https://test.com/%s">Test %s</a>
-		</div>
-		<div class="card">
-			<a href="https://wronglink.com">Wrong Link</a>
-		</div>
-	</div>
-</html>`, phrase, phrase, phrase, phrase)
+				io.WriteString(w, "<html><body>")
+				io.WriteString(w, "<div class='container'>")
+
+				for _, link := range links {
+					fmt.Fprintf(w, "%s", testCase.top)
+					io.WriteString(w, link)
+					fmt.Fprintf(w, "%s", testCase.bottom)
+				}
+
+				io.WriteString(w, "</div>")
+				io.WriteString(w, "</body></html>")
 			}))
 			defer testServer.Close()
-			got, _ := scraper.FetchItems(testServer.URL, "example test")
-			want := []scraper.Link{
-				{"Example", "https://example.com"},
-				{"Example " + phrase, "https://example.com/" + phrase},
-				{"Test", "https://test.com"},
-				{"Test " + phrase, "https://test.com/" + phrase},
-			}
 
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("\ngot %q,\nwant %q", got, want)
-			}
-		}
-	})
+			got, err := scraper.FetchItems(testServer.URL, testCase.cardPath, "example test")
 
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			} else if !reflect.DeepEqual(got, testCase.want) {
+				t.Errorf("\ngot:\n%q\nwant:\n%q", got, testCase.want)
+			}
+		})
+	}
 	t.Run("Return error if request fails", func(t *testing.T) {
 		errorCases := map[string]int{
-			"403: Forbidden":          http.StatusForbidden,
-			"401: Unauthorized":       http.StatusUnauthorized,
-			"400: Bad Request":        http.StatusBadRequest,
-			"404: Not Found":          http.StatusNotFound,
-			"408: Request Timeout":    http.StatusRequestTimeout,
-			"405: Method Not Allowed": http.StatusMethodNotAllowed,
-			"429: Too Many Requests":  http.StatusTooManyRequests,
+			"Forbidden":          http.StatusForbidden,
+			"Unauthorized":       http.StatusUnauthorized,
+			"Bad Request":        http.StatusBadRequest,
+			"Not Found":          http.StatusNotFound,
+			"Request Timeout":    http.StatusRequestTimeout,
+			"Method Not Allowed": http.StatusMethodNotAllowed,
+			"Too Many Requests":  http.StatusTooManyRequests,
 		}
 		for want, status := range errorCases {
 			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -66,7 +76,7 @@ func TestFetchData(t *testing.T) {
 			}))
 			defer testServer.Close()
 
-			_, got := scraper.FetchItems(testServer.URL, "")
+			_, got := scraper.FetchItems(testServer.URL, "", "")
 
 			if got.Error() != want {
 				t.Errorf("got %q, want %q", got, want)
