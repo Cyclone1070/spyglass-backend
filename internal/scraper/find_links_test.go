236a4ebb77af3f41d97f7c9247774e55da16d1f9
Hoang Mai <128883620+Cyclone1070@users.noreply.github.com>
2025-06-27 20:52:59 +1000

    added all categories for scraping

--- internal/scraper/find_links_test.go
+++ internal/scraper/find_links_test.go
@@ -14,13 +14,63 @@ import (
 func TestFindLinks(t *testing.T) {
 	testCases := []struct {
 		description string
-		ids          []string
-		catagory    string
+		selectors   []string
+		category    string
 	}{
 		{
-			"list of ebooks catagory",
+			"list of ebooks category",
 			[]string{"ebooks", "public-domain", "pdf-search"},
 			"Books",
+		},
+		{
+			"list of movies category",
+			[]string{"streaming-sites", "free-w-ads", "anime-streaming"},
+			"Movies",
+		},
+		{
+			"list of games download category",
+			[]string{"download-games"},
+			"Games Download",
+		},
+		{
+			"list of games repack category",
+			[]string{"repack-games"},
+			"Games Repack",
+		},
+		{
+			"list of abandonware/ROM category",
+			[]string{"abandonware-retro", "rom-sites", "nintendo-roms", "sony-roms"},
+			"Abandonware/ROM",
+		},
+		{
+			"list of mac games category",
+			[]string{"mac-gaming"},
+			"Mac Games",
+		},
+		{
+			"list of linux games category",
+			[]string{"linux-gaming"},
+			"Linux Games",
+		},
+		{
+			"list of windows software category",
+			[]string{"software-sites"},
+			"Windows Software",
+		},
+		{
+			"list of mac software category",
+			[]string{"software-sites-1"},
+			"Mac Software",
+		},
+		{
+			"list of android apps category",
+			[]string{"modded-apks", "untouched-apks"},
+			"Android apps",
+		},
+		{
+			"list of ios apps category",
+			[]string{"ios-ipas"},
+			"IOS apps",
 		},
 	}
 
@@ -28,7 +78,7 @@ func TestFindLinks(t *testing.T) {
 		t.Run(testCase.description, func(t *testing.T) {
 			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				io.WriteString(w, "<html><body>")
-				for _, id := range testCase.ids {
+				for _, id := range testCase.selectors {
 					fmt.Fprintf(
 						w,
 						`<h2 id="%[1]s">%[1]s</h2>
@@ -54,8 +104,8 @@ func TestFindLinks(t *testing.T) {
 			got, err := scraper.FindLinks(testServer.URL)
 
 			want := []scraper.Link{}
-			for _, id := range testCase.ids {
-				want = append(want, scraper.Link{"Link 1 " + id, "https://link1" + id + ".com", testCase.catagory}, scraper.Link{"Link 2 " + id, "https://link2" + id + ".com", testCase.catagory})	
+			for _, id := range testCase.selectors {
+				want = append(want, scraper.Link{"Link 1 " + id, "https://link1" + id + ".com", testCase.category}, scraper.Link{"Link 2 " + id, "https://link2" + id + ".com", testCase.category})
 			}
 
 			if err != nil {
