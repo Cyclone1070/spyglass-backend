8b8099abda3d44a6cbf3541b66d45024b0a28581
Hoang Mai <128883620+Cyclone1070@users.noreply.github.com>
2025-06-27 20:52:59 +1000

    added all categories for scraping

--- internal/scraper/find_links.go
+++ internal/scraper/find_links.go
@@ -9,17 +9,27 @@ func FindLinks(url string) ([]Link, error) {
 	collector := colly.NewCollector()
 	var err error
 	links := []Link{}
-
-	collector.OnHTML("#ebooks, #public-domain, #pdf-search", func(category *colly.HTMLElement) {
-		// Get the main link text and URL
-		category.DOM.NextFiltered("ul").Find("li.starred strong a").Each(func(_ int, e *goquery.Selection) {
-			linkName := e.Text()
-			linkURL, exists := e.Attr("href")
-			if exists {
-				links = append(links, Link{linkName, linkURL, "Books"})
-			}
+	categories := []struct{
+		name string
+		selector string
+	} {
+		{"Books", "#ebooks, #public-domain, #pdf-search"},
+		{"Movies", "#streaming-sites, #free-w-ads, #anime-streaming"},
+		{"Games Download", "#download-games"},
+		{"Games Repack", "#repack-games"},
+		{"Abandonware/ROM", "#abandonware-retro, #rom-sites, #nintendo-roms, #sony-roms"},
+		{"Mac Games", "#mac-gaming"},
+		{"Linux Games", "#linux-gaming"},
+		{"Windows Software", "#software-sites"},
+		{"Mac Software", "#software-sites-1"},
+		{"Android apps", "#modded-apks, #untouched-apks"},
+		{"IOS apps", "#ios-ipas"},
+	}
+	for _, category := range categories {
+		collector.OnHTML(category.selector, func(categoryHTML *colly.HTMLElement) {
+			scrapeLinkFromCategory(categoryHTML, category.name, &links)
 		})
-	})
+	}
 
 	collector.OnError(func(r *colly.Response, e error) {
 		err = e
@@ -28,4 +38,15 @@ func FindLinks(url string) ([]Link, error) {
 	collector.Visit(url)
 
 	return links, err
+}
+
+// helper function to scrape links from a category html element
+func scrapeLinkFromCategory(category *colly.HTMLElement, categoryName string, links *[]Link) {
+	category.DOM.NextFiltered("ul").Find("li.starred strong a").Each(func(_ int, e *goquery.Selection) {
+		linkName := e.Text()
+		linkURL, exists := e.Attr("href")
+		if exists {
+			*links = append(*links, Link{linkName, linkURL, categoryName})
+		}
+	})
 }
