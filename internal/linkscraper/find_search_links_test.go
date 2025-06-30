ffdff9c4c5891b13b47169691bd9b9b137903974
Hoang Mai <128883620+Cyclone1070@users.noreply.github.com>
2025-06-30 19:04:14 +1000

    added multiple search URLs error

--- internal/linkscraper/find_search_links_test.go
+++ internal/linkscraper/find_search_links_test.go
@@ -44,5 +44,28 @@ func TestFindSearchLinks(t *testing.T) {
 				assertEqual(want.Error(), err.Error(), t)
 			}
 		})
+		t.Run("return error when multiple search urls are found", func(t *testing.T) {
+			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				io.WriteString(w, "<html><body>")
+				io.WriteString(
+					w,
+					`<form action="/search1" method="get">
+						<input name="q1" type="search"/>
+					</form>
+					<form action="/search2" method="get">
+						<input name="q2" type="search"/>
+					</form>`,
+				)
+				io.WriteString(w, "</body></html>")
+			}))
+			defer testServer.Close()
+			want := errors.New("multiple search URLs found")
+			_, err := linkscraper.FindSearchLinks(linkscraper.WebsiteLink{"test title", testServer.URL, "test category"})
+			if err == nil {
+				t.Fatalf("expected error %q, got no error", want)
+			} else {
+				assertEqual(want.Error(), err.Error(), t)
+			}
+		})
 	}
 }
