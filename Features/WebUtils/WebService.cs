a119030c47d21d8024bf0fcebe40623ca6d2b5e2
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2025-09-24 01:52:13 +1000

    small fixes

--- Features/WebUtils/WebService.cs
+++ Features/WebUtils/WebService.cs
@@ -21,7 +21,6 @@ namespace spyglass_backend.Features.WebUtils
 			client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
 			client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
 			client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US,en;q=0.9");
-			client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
 			client.DefaultRequestHeaders.TryAddWithoutValidation("Connection", "keep-alive");
 			client.DefaultRequestHeaders.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
 			client.DefaultRequestHeaders.TryAddWithoutValidation("DNT", "1");
